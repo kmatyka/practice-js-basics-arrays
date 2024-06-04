1e2f50e64d7d39e832b05ed9c653a602dee95183
Kamil <kmatyka.15012004@gmail.com>
2024-06-04 21:23:01 +0200

    zadanie 1

---
+++ 01/app.js
@@ -0,0 +1,9 @@
+users = [];
+users.push("Kamil Bąk");
+users.push("Bartek Bożek");
+users.push("Robert Lewandowski");
+users.push("Marcin Tworek");
+users.push("Michał Dąbrowski");
+
+console.log(users[0]+", "+users[2]+", "+users[4]);
+console.log(users.length);
