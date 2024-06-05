862e498be6e9bcd2b1fd37d3e0478714c2d768f0
Kamil <kmatyka.15012004@gmail.com>
2024-06-05 15:34:03 +0200

    zadania

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,6 @@
 const n = 24;
 const oddNumbers = [];
+for(let i=1;i<=n;i+=2){
+    oddNumbers.push(i);
+}
+console.log(oddNumbers);
--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,4 @@
 const years = [1980, 1934, 2002, 2019];
+let now = 2024;
+const since = years.map(el => now - el);
+console.log(since);
